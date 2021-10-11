b19f02411830610dfa2ee5dcf0e418dc52c0e632
asmrayat <asmrayat@gmail.com>
2021-10-12 03:47:45 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './Component/Home/Home';
+import Login from './Component/Login/Login';
+import Register from './Component/Register/Register';
+import Header from './Component/Home/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+
+          <Route path="/register">
+            <Register></Register>
+          </Route>
+
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
